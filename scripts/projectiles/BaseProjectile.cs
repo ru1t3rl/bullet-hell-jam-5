e0454e71c1c5dc11c2ab710b55974a9bc5c638b9
ru1t3rl <lucaruiters@hotmail.nl>
2024-05-11 12:09:00 +0200

    Add base projectile behaviour

--- scripts/projectiles/BaseProjectile.cs
+++ scripts/projectiles/BaseProjectile.cs
@@ -1,15 +1,71 @@
 using Godot;
-using System;
+
+namespace BulletHellJam5.projectiles;
 
 public partial class BaseProjectile : Area2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    [Export]
+    private float _speed;
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    [Export(hintString: "Lifespan in seconds")]
+    private float _lifeSpan = 10;
+
+    private Vector2 _velocity = Vector2.Zero;
+
+    private Timer _lifespanTimer = new();
+
+    public override void _Ready()
+    {
+        _lifespanTimer.Timeout += LifespanTimerOnTimeout;
+    }
+
+    private void LifespanTimerOnTimeout()
+    {
+        Visible = false;
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        GlobalPosition += _velocity * (float)delta;
+        EdgeCheck();
+    }
+
+    public void Fire(Vector2 direction)
+    {
+        _velocity = direction.Normalized() * _speed;
+        _lifespanTimer.Start();
+    }
+
+    private void EdgeCheck()
+    {
+        var viewport = GetViewportRect();
+        var position = GlobalPosition;
+
+        position.X = position.X switch
+        {
+            _ when position.X > viewport.Size.X => 0,
+            _ when position.X < 0 => viewport.Size.X,
+            _ => position.X
+        };
+
+        position.Y = position.Y switch
+        {
+            _ when position.Y > viewport.Size.Y => 0,
+            _ when position.Y < 0 => viewport.Size.Y,
+            _ => position.Y
+        };
+    }
+
+    protected void TruncateVelocity()
+    {
+        float sqrMagnitude = _velocity.X * _velocity.X + _velocity.Y * _velocity.Y;
+
+        if (sqrMagnitude <= _speed * _speed)
+        {
+            return;
+        }
+
+        _velocity = _velocity.Normalized();
+        _velocity *= _speed;
+    }
 }
