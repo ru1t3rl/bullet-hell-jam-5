08f28ffb7d646cb246807a4fc6707d7c42c7fd4a
ru1t3rl <lucaruiters@hotmail.nl>
2024-05-11 10:54:29 +0200

    Add simple states to the enemy

--- scripts/enemies/base/BaseEnemy.cs
+++ scripts/enemies/base/BaseEnemy.cs
@@ -1,3 +1,4 @@
+using BulletHellJam5.Enemies.Enums;
 using Godot;
 
 namespace BulletHellJam5.Enemies;
@@ -6,28 +7,30 @@ public abstract partial class BaseEnemy : CharacterBody2D
 {
     [Export]
     protected float _health;
-
     public float Health => _health;
 
     [Export]
     protected float _speed;
-
-    protected Vector2 _velocity;
+    protected Vector2 _velocity = Vector2.Zero;
 
     [Export(PropertyHint.Range, "0, 1, 0.1f")]
     protected float _powerupDropchange = 0.5f;
 
-    protected abstract void Move();
-
-    public virtual void Shoot()
-    {
-        EmitSignal(nameof(OnShootEventHandler));
-    }
+    private EnemyState _state = EnemyState.Idle;
+    public EnemyState State => _state;
 
     public override void _PhysicsProcess(double delta)
     {
         Move();
         MoveAndSlide();
+    }
+
+    protected abstract void Move();
+
+    public virtual void Shoot()
+    {
+        _state = EnemyState.Shooting;
+        EmitSignal(nameof(OnShootEventHandler));
     }
 
     public virtual void TakeDamage(float damage)
