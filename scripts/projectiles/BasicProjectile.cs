ef54c8231cb8cd42c9b66e4129f5862d79dd5053
johnathan <johnathan.claasen@hva.nl>
2024-05-20 16:27:22 +0200

    Fixed player positioning and screenshake

--- scripts/projectiles/BasicProjectile.cs
+++ scripts/projectiles/BasicProjectile.cs
@@ -5,27 +5,27 @@ namespace BulletHellJam5.projectiles;
 
 public partial class BasicProjectile : BaseProjectile
 {
-    protected override void Move(double delta)
-    {
-        GlobalPosition += Velocity * (float)delta;
-    }
+	protected override void Move(double delta)
+	{
+		GlobalPosition += Velocity * (float)delta;
+	}
 
-    protected override void OnCollisionWithBody(Node2D body)
-    {
-        base.OnCollisionWithBody(body);
+	protected override void OnCollisionWithBody(Node2D body)
+	{
+		base.OnCollisionWithBody(body);
 
-        if (body is Player player)
-        {
-            this.SetCollisionLayerValue(1, false);
-            this.SetCollisionMaskValue(1, false);
-            //^ Sets 1st layer to false (Cannot collide with player again)
-            //v Sets 2nd layer to true (Can collide with enemy)
-            this.SetCollisionLayerValue(2, true);
-            this.SetCollisionMaskValue(2, true);
+		if (body is Player player)
+		{
+			this.SetCollisionLayerValue(1, false);
+			this.SetCollisionMaskValue(1, false);
+			//^ Sets 1st layer to false (Cannot collide with player again)
+			//v Sets 2nd layer to true (Can collide with enemy)
+			this.SetCollisionLayerValue(2, true);
+			this.SetCollisionMaskValue(2, true);
 
-            Vector2 incident = Velocity.Normalized();
-            Vector2 reflected = incident - 2 * (incident.Dot(player.Normal)) * player.Normal;
-            Velocity = reflected * Velocity.Length(); // Calculate reflected velocity once
-        }
-    }
+			Vector2 incident = Velocity.Normalized();
+			Vector2 reflected = incident - 2 * (incident.Dot(player.Normal)) * player.Normal;
+			Velocity = reflected * Velocity.Length(); // Calculate reflected velocity once
+		}
+	}
 }
--- scripts/resource-zones/BaseResourceZone.cs
+++ scripts/resource-zones/BaseResourceZone.cs
@@ -6,150 +6,150 @@ namespace BulletHellJam5.ResourceZones;
 
 public partial class BaseResourceZone : Area2D
 {
-    [Export]
-    private Currency _currency;
+	[Export]
+	private Currency _currency;
 
-    [Export]
-    private int _health = 50;
-    private int _startHealth;
+	[Export]
+	private int _health = 50;
+	private int _startHealth;
 
-    [ExportGroup("Resource Generation")]
-    [Export]
-    private int _numberOfResourcesPerGeneration = 10;
-    [Export]
-    private float _resourceGenerationInterval = 5f;
-    private Timer _resourceGenerationTimer = new();
+	[ExportGroup("Resource Generation")]
+	[Export]
+	private int _numberOfResourcesPerGeneration = 10;
+	[Export]
+	private float _resourceGenerationInterval = 5f;
+	private Timer _resourceGenerationTimer = new();
 
-    private int _currentNumberOfResources;
+	private int _currentNumberOfResources;
 
-    private Polygon2D _polygon;
-    private TextEdit _healthText;
+	private Polygon2D _polygon;
+	private TextEdit _healthText;
 
-    public override void _Ready()
-    {
-        _polygon = GetNode<Polygon2D>("Polygon2D");
-        _healthText = GetNode<TextEdit>("HealthText");
-        if (_currency is null)
-        {
-            GD.Print("Linked supply resources isn't a valid supply");
-            return;
-        }
+	public override void _Ready()
+	{
+		_polygon = GetNode<Polygon2D>("Polygon2D");
+		_healthText = GetNode<TextEdit>("HealthText");
+		if (_currency is null)
+		{
+			GD.Print("Linked supply resources isn't a valid supply");
+			return;
+		}
 
-        SetupTimer();
+		SetupTimer();
 
-        _startHealth = _health;
-    }
+		_startHealth = _health;
+	}
 
 
-    private void _on_area_entered(Area2D area)
-    {
-        EmitSignal(nameof(OnCollision), area);
-        if (area is not BaseProjectile projectile)
-        {
-            return;
-        }
+	private void _on_area_entered(Area2D area)
+	{
+		EmitSignal(nameof(OnCollision), area);
+		if (area is not BaseProjectile projectile)
+		{
+			return;
+		}
 
-        TakeDamage(projectile.Damage);
-    }
+		TakeDamage(projectile.Damage);
+	}
 
-    private void OnBodyEntered(Node2D body)
-    {
-        EmitSignal(nameof(OnCollision), body);
-        if (body is not BaseProjectile projectile)
-        {
-            return;
-        }
+	private void OnBodyEntered(Node2D body)
+	{
+		EmitSignal(nameof(OnCollision), body);
+		if (body is not BaseProjectile projectile)
+		{
+			return;
+		}
 
-        TakeDamage(projectile.Damage);
-    }
+		TakeDamage(projectile.Damage);
+	}
 
-    public void TakeDamage(int amount)
-    {
-        _health -= amount;
-        EmitSignal(nameof(OnTakeDamage), amount);
-        UpdateColor();
-        UpdateText();
-        HealthCheck();
-    }
+	public void TakeDamage(int amount)
+	{
+		_health -= amount;
+		EmitSignal(nameof(OnTakeDamage), amount);
+		UpdateColor();
+		UpdateText();
+		HealthCheck();
+	}
 
-    //TODO for some reason health is still going below 0
-    private void HealthCheck()
-    {
-        if (_health <= 0)
-        {
-            _health = 0;
-            Die();
-        }
-    }
+	//TODO for some reason health is still going below 0
+	private void HealthCheck()
+	{
+		if (_health <= 0)
+		{
+			_health = 0;
+			Die();
+		}
+	}
 
-    protected virtual void Die()
-    {
-        EmitSignal(nameof(OnZoneDestroyed), 1);
-        QueueFree();
-    }
+	protected virtual void Die()
+	{
+		EmitSignal(nameof(OnZoneDestroyed), 1);
+		QueueFree();
+	}
 
-    public void TakeResources(int amount)
-    {
-        _currentNumberOfResources -= amount;
-        EmitSignal(nameof(OnTakeResources), amount);
-    }
+	public void TakeResources(int amount)
+	{
+		_currentNumberOfResources -= amount;
+		EmitSignal(nameof(OnTakeResources), amount);
+	}
 
-    public int TakeResourcesHealthBased()
-    {
-        int resourcesToTake = Mathf.RoundToInt(_currentNumberOfResources * (_health / (float)_startHealth));
-        _currentNumberOfResources -= resourcesToTake;
+	public int TakeResourcesHealthBased()
+	{
+		int resourcesToTake = Mathf.RoundToInt(_currentNumberOfResources * (_health / (float)_startHealth));
+		_currentNumberOfResources -= resourcesToTake;
 
-        EmitSignal(nameof(OnTakeResources), 1);
-        return resourcesToTake;
-    }
+		EmitSignal(nameof(OnTakeResources), 1);
+		return resourcesToTake;
+	}
 
-    public int TakeAllResources()
-    {
-        var nResources = _currentNumberOfResources;
-        _currentNumberOfResources = 0;
+	public int TakeAllResources()
+	{
+		var nResources = _currentNumberOfResources;
+		_currentNumberOfResources = 0;
 
-        EmitSignal(nameof(OnTakeResources), nResources);
-        return nResources;
-    }
+		EmitSignal(nameof(OnTakeResources), nResources);
+		return nResources;
+	}
 
-    private void SetupTimer()
-    {
-        AddChild(_resourceGenerationTimer);
-        _resourceGenerationTimer.WaitTime = _resourceGenerationInterval;
-        _resourceGenerationTimer.Timeout += OnResourceTimerTick;
-        _resourceGenerationTimer.Start();
-    }
+	private void SetupTimer()
+	{
+		AddChild(_resourceGenerationTimer);
+		_resourceGenerationTimer.WaitTime = _resourceGenerationInterval;
+		_resourceGenerationTimer.Timeout += OnResourceTimerTick;
+		_resourceGenerationTimer.Start();
+	}
 
-    private void UpdateText()
-    {
-        _healthText.Text = _health.ToString();
-    }
+	private void UpdateText()
+	{
+		_healthText.Text = _health.ToString();
+	}
 
-    private void OnResourceTimerTick()
-    {
-        _currentNumberOfResources += _numberOfResourcesPerGeneration;
-        EmitSignal(nameof(OnResourcesGenerated));
-    }
+	private void OnResourceTimerTick()
+	{
+		_currentNumberOfResources += _numberOfResourcesPerGeneration;
+		EmitSignal(nameof(OnResourcesGenerated));
+	}
 
-    private void UpdateColor()
-    {
-        if (_polygon == null)
-        {
-            return;
-        }
+	private void UpdateColor()
+	{
+		if (_polygon == null)
+		{
+			return;
+		}
 
-        // Calculate the new color based on the health
-        float healthPercentage = _health / (float)_startHealth;
-        Color currentColor = _polygon.Color;
+		// Calculate the new color based on the health
+		float healthPercentage = _health / (float)_startHealth;
+		Color currentColor = _polygon.Color;
 
-        // Set alpha to 0 if health is less than or equal to 0
-        float newAlpha = _health <= 0
-            ? 0
-            : currentColor.A;
+		// Set alpha to 0 if health is less than or equal to 0
+		float newAlpha = _health <= 0
+			? 0
+			: currentColor.A;
 
-        Color newColor =
-            new Color(1.0f, healthPercentage, healthPercentage, newAlpha); // Preserve or update alpha value
+		Color newColor =
+			new Color(1.0f, healthPercentage, healthPercentage, newAlpha); // Preserve or update alpha value
 
-        _polygon.Color = newColor;
-    }
+		_polygon.Color = newColor;
+	}
 }
