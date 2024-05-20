4c674ace75ea587cf4884c7c35300e7dba0317d2
ru1t3rl <lucaruiters@hotmail.nl>
2024-05-20 13:52:23 +0200

    Rewrite player in c# instead of GDscript

---
+++ scripts/Player/Player.cs
@@ -0,0 +1,61 @@
+using Godot;
+
+public partial class Player : CharacterBody2D
+{
+    [Export]
+    private float _distanceFromCenter = 5f;
+
+    [Export]
+    private float _speed = 100f;
+
+    private Vector2 _input = Vector2.Zero;
+    private Vector2 _viewportSize = Vector2.Zero;
+
+    public override void _Ready()
+    {
+        Vector2 direction = new Vector2(
+            Mathf.Cos(Rotation),
+            Mathf.Sin(Rotation)
+        );
+
+        GlobalPosition = _distanceFromCenter * direction + _viewportSize / 2;
+    }
+
+    public override void _Process(double delta)
+    {
+        _viewportSize = GetViewportRect().Size;
+        _input = GetInput();
+
+        if (_input is { X: 0, Y: 0 })
+        {
+            return;
+        }
+
+        Rotation += _speed * _input.X * (float)delta;
+
+
+        GlobalPosition = _distanceFromCenter * Normal + _viewportSize / 2;
+    }
+
+    public Vector2 Normal => new(
+        Mathf.Cos(Rotation),
+        Mathf.Sin(Rotation)
+    );
+
+    private Vector2 GetInput()
+    {
+        Vector2 input = Vector2.Zero;
+
+        if (Input.IsActionPressed("Rotate_Clockwise"))
+        {
+            input.X += 1;
+        }
+
+        if (Input.IsActionPressed("Rotate_CounterClockwise"))
+        {
+            input.X -= 1;
+        }
+
+        return input;
+    }
+}
